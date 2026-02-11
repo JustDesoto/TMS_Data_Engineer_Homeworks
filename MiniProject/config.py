77a7555b6a5eac3a86038af407847d854a0c687d
JustDesoto <forsakensif@gmail.com>
2026-02-11 23:51:50 +0300

    fix logger console display

--- MiniProject/config.py
+++ MiniProject/config.py
@@ -1,7 +1,7 @@
 # CONFIGURATION
 # Т.к. проект реализован не в отдельном репозитории, указываем путь, с учётом корневой папки
 
-PROJECT_PATH = "MiniProject/" # Наименование папки с проектом
+PROJECT_PATH = "MiniProject\\" # Наименование папки с проектом
 
 DB_PATH = f"{PROJECT_PATH}chinook.db"  # Наименование базы-источника в корне проекта
 DATA_EXPORT_FOLDER = f"{PROJECT_PATH}data"  # Наименование папки для выгрузки данных
