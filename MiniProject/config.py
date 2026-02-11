e06ad1ccd92d84416263863d6b3eb62047da6e68
JustDesoto <forsakensif@gmail.com>
2026-02-11 23:46:04 +0300

    fix config + delete data

--- MiniProject/config.py
+++ MiniProject/config.py
@@ -1,6 +1,8 @@
 # CONFIGURATION
 # Т.к. проект реализован не в отдельном репозитории, указываем путь, с учётом корневой папки
 
-DB_PATH = "MiniProject/chinook.db"  # Наименование базы-источника в корне проекта
-DATA_EXPORT_FOLDER = "MiniProject/data"  # Наименование папки для выгрузки данных
-TASK_EXPORT_FOLDER = "MiniProject/result"  # Наименование папки для выгрузки результатов аналитки
+PROJECT_PATH = "MiniProject/" # Наименование папки с проектом
+
+DB_PATH = f"{PROJECT_PATH}chinook.db"  # Наименование базы-источника в корне проекта
+DATA_EXPORT_FOLDER = f"{PROJECT_PATH}data"  # Наименование папки для выгрузки данных
+TASK_EXPORT_FOLDER = f"{PROJECT_PATH}result"  # Наименование папки для выгрузки результатов аналитки
